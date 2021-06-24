cc9f9c68734ae9d680bc144894bdff2a474a5143
mega <mega.mor@gmail.com>
2021-06-24 13:16:02 +0300

    use argparse to provide the configuration path

--- cli.py
+++ cli.py
@@ -1,12 +1,23 @@
+
+import argparse
 import time
 from pathlib import Path
 from train import Trainer
 from utils.common import create_time_taken_string
 
 
-config_path = Path("config/costanzo_hu_krogan.json")
-time_start = time.time()
-trainer = Trainer(config_path)
-trainer.train()
-trainer.forward()
-time_end = time.time()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_path", type=str, required=True)
+    args = parser.parse_args()
+
+    config_path = Path(args.config_path)
+    time_start = time.time()
+    trainer = Trainer(config_path)
+    trainer.train()
+    trainer.forward()
+    time_end = time.time()
+
+
+if __name__ == '__main__':
+    main()
